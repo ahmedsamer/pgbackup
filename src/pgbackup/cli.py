e67f0f8898d1c11db869718c3907d0a476bdc9d4
Ahmed Samir <ahmed.samer@gmail.com>
2019-01-10 01:09:55 +0200

    Create CLI, DUMP and STORAGE Modules.

---
+++ src/pgbackup/cli.py
@@ -0,0 +1,37 @@
+from argparse import Action, ArgumentParser
+
+known_drivers = ['local', 's3']
+
+
+class DriverAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error('Unknown driver, use "local" or "s3"')
+        namespace.driver = driver.lower()
+        namespace.destination = destination
+
+
+def create_parser():
+    parser = ArgumentParser()
+    parser.add_argument('url', help='url of the postgres database')
+    parser.add_argument('--driver',
+                        help='How and where to store your backup',
+                        nargs=2,
+                        action=DriverAction,
+                        required=True)
+    return parser
+
+
+def main():
+    import boto3
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        storage.s3(client, dump.stdout, args.destination, 'example.sql')
+    else:
+        outfile = open(args.destination, 'wb')
+        storage.local(dump.stdout, outfile)
