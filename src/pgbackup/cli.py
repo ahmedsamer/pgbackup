d8bcbb9bf7e18bf5d878181dcaf4f7517862058e
Ahmed Samir <ahmed.samer@gmail.com>
2019-01-10 09:03:35 +0200

    Enhancements: add timestamp to s3 backups.

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -15,15 +15,17 @@ class DriverAction(Action):
 def create_parser():
     parser = ArgumentParser()
     parser.add_argument('url', help='url of the postgres database')
-    parser.add_argument('--driver',
+    parser.add_argument('--driver', '-d',
                         help='How and where to store your backup',
                         nargs=2,
                         action=DriverAction,
+                        metavar=('driver', 'destination'),
                         required=True)
     return parser
 
 
 def main():
+    import time
     import boto3
     from pgbackup import pgdump, storage
 
@@ -31,7 +33,11 @@ def main():
     dump = pgdump.dump(args.url)
     if args.driver == 's3':
         client = boto3.client('s3')
-        storage.s3(client, dump.stdout, args.destination, 'example.sql')
+        timestamp = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing database to {args.destination} in S3 as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
     else:
         outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {args.destination}")
         storage.local(dump.stdout, outfile)
--- tests/test_pgdump.py
+++ tests/test_pgdump.py
@@ -3,7 +3,7 @@ import pytest
 
 from pgbackup import pgdump
 
-url = "postgres://user:password@example.com:5432/mydb"
+url = "postgres://user:password@example.com:5432/db_name"
 
 
 def test_dump_calls_pg_dump(mocker):
@@ -23,3 +23,18 @@ def test_dump_handles_os_error(mocker):
     mocker.patch('subprocess.Popen', side_effect=OSError("no such file"))
     with pytest.raises(SystemExit):
         pgdump.dump(url)
+
+
+def test_dump_filname_without_timestamp():
+    """
+    return the name of database
+    """
+    assert pgdump.dump_file_name(url) == "db_name.sql"
+
+
+def test_dump_filname_with_timestamp():
+    """
+    return the name of database
+    """
+    timestamp = "2019-01-10_08:12:20"
+    assert pgdump.dump_file_name(url, timestamp) == f"db_name-{timestamp}.sql"
